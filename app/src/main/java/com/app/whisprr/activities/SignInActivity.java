4083c934947534c9802569bd81843edce63da099
Ayan Babwany <babwany@ualberta.ca>
2023-05-13 20:47:00 -0600

    Set up firebase configuration, created theme and setup theme colors

--- app/src/main/java/com/app/whisprr/activities/SignInActivity.java
+++ app/src/main/java/com/app/whisprr/activities/SignInActivity.java
@@ -1,14 +1,16 @@
-package com.app.whisprr;
+package com.app.whisprr.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.app.whisprr.R;
+
+public class SignInActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_sign_in);
     }
 }
